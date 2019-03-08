d613d56bf77761fbec6d420855bd39edecb7ba2e
Pablo Garcia <pgarcia@dragoons.gg>
2019-03-08 00:18:39 -0300

    Changed namespace from development to jx-staging

--- install-dev/helm-apply.sh
+++ install-dev/helm-apply.sh
@@ -2,8 +2,8 @@
 # Files are ordered in proper order with needed wait for the dependent custom resource definitions to get initialized.
 # Usage: bash helm-apply.sh
 # kubectl apply -f namespace.yaml
-kubectl label namespace development istio-injection=enabled
+kubectl label namespace jx-staging istio-injection=enabled
 
 helm delete --purge csvc 2>/dev/null
 helm dep up ../csvc
-helm install --name csvc ../csvc --namespace development
+helm install --name csvc ../csvc --namespace jx-staging
